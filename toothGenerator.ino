c0fceec642957f6e1f91c92ddbe632564d03bd86
jmsmuy <jmsmuy@gmail.com>
2018-06-25 23:56:48 -0300

    First commit

---
+++ toothGenerator.ino
@@ -0,0 +1,75 @@
+#define DEBOUNCE_THRESH 500
+#define CASE_1 // this is ref tooth same as small tooth and aligned
+#define CASE_2 // this is ref tooth same as small tooth but not aligned
+#define CASE_3 // this is ref tooth double size, start aligned
+#define CASE_4 // this is ref tooth double size, start misaligned
+
+long rpm = 6500;
+long toothPerCycle = 116;
+long toothPerCycleTimes2 = toothPerCycle * 2 - 1;
+double fixCoef = 0.94;
+long delayMicroSecondsBetweenSmallTooth = (1000000l / (rpm * toothPerCycle * 2l / 60l)) * fixCoef;
+int toothCounter = 0;
+long lastButtonTime = 0;
+boolean changeDetected = false;
+
+void recalculate() {
+  toothPerCycleTimes2 = toothPerCycle * 2 - 1;
+  delayMicroSecondsBetweenSmallTooth = (1000000l / (rpm * toothPerCycle * 2l / 60l)) * fixCoef;
+}
+
+void button() {
+  long currentTime = millis();
+  if (lastButtonTime < currentTime - DEBOUNCE_THRESH) {
+    lastButtonTime = currentTime;
+    if (rpm >= 6500) {
+      rpm = 800;
+    } else {
+      rpm += 500;
+    }
+    recalculate();
+  }
+  changeDetected = true;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  DDRB = B11000000;
+  PORTB = B11000000;
+  toothCounter = 0;
+  Serial.begin(9600);
+  Serial.println(delayMicroSecondsBetweenSmallTooth);
+
+  pinMode(2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), button, FALLING);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delayMicroseconds(delayMicroSecondsBetweenSmallTooth);
+  toothCounter++;
+  #if CASE_1
+    if (toothCounter >= toothPerCycleTimes2) {
+      PORTB = B11000000;
+      toothCounter = -1;
+    } else {
+      PORTB = ~(PORTB) & B10000000;
+    }
+  #elif CASE_2
+    if (toothCounter >= toothPerCycleTimes2) {
+      PORTB = B11000000;
+      toothCounter = -1;
+    } else {
+      PORTB = ~(PORTB) & B10000000;
+    }
+  #elif CASE_3
+  
+  #elif CASE_4
+  
+  #endif
+  if(changeDetected){
+    Serial.println("Change detected");
+    Serial.println(rpm);
+    changeDetected = false;
+  }
+}
